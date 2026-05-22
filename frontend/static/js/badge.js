46d89ce04e9bdf2bfc76574b5116297525695b3e
Kerimniy <mutalupkerim@gmail.com>
2026-05-22 15:12:08 +0300

    hotfix: round uptime fix

--- frontend/static/js/badge.js
+++ frontend/static/js/badge.js
@@ -1,4 +1,5 @@
 let badges = document.querySelectorAll("[data-badge-url]")
+
 let timestamp = Date.now();
 function p2rgb(v){
     let g
@@ -52,6 +53,8 @@ for (let badge of badges){
         uptime=1  
       }
 
+	  uptime=Math.round(uptime*100)
+
       let badgSVG = `
       
         <svg xmlns="http://www.w3.org/2000/svg" width="130" height="20" role="img" aria-label="h24uptime">
@@ -65,14 +68,14 @@ for (let badge of badges){
             </clipPath>
             <g clip-path="url(#r)">
                 <rect width="80" height="20" fill="#4d4d4dff"/>
-                <rect x="77" width="50" height="20" fill=${p2rgb(uptime)}>
+                <rect x="77" width="50" height="20" fill=${p2rgb(uptime/100)}>
                 <rect width="130" height="20" fill="url(/)"/>
             </g>
             <g fill="#fff" text-anchor="middle" font-family="Verdana,Geneva,DejaVu Sans,sans-serif" text-rendering="geometricPrecision" font-size="110">
                 <text aria-hidden="true" x="395" y="150" fill="#010101" fill-opacity=".3" transform="scale(.1)" textLength="700">Uptime (24h)</text>
                 <text x="395" y="140" transform="scale(.1)" fill="#fff" textLength="700">Uptime (24h)</text>
-                <text aria-hidden="true" x="1000" y="150" fill="#010101" fill-opacity=".3" transform="scale(.1)">${uptime*100}%</text>
-                <text x="1000" y="140" transform="scale(.1)" fill="#fff" >${uptime*100}%</text>
+                <text aria-hidden="true" x="1000" y="150" fill="#010101" fill-opacity=".3" transform="scale(.1)">${uptime}%</text>
+                <text x="1000" y="140" transform="scale(.1)" fill="#fff" >${uptime}%</text>
             </g>
         </svg>
       
